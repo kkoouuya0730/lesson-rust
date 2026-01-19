e0dfc374c7ecaa41f65de5fba58c002e544d9979
kkoouuya0730 <kouya.0730@icloud.com>
2026-01-19 22:57:27 +0900

    シャドーイング

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+// シャドーイング
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x); // 12
+    }
+
+    println!("The value of x is: {}", x); // 6
 }
