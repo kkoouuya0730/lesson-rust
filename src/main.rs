ce3ce0a8c0ec920090486f7a5612c37a78f1605d
kkoouuya0730 <kouya.0730@icloud.com>
2026-01-20 22:10:38 +0900

    データ型

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,48 @@
-// シャドーイング
+// データ型
+
+use std::io;
+
 fn main() {
-    let x = 5;
-    let x = x + 1;
+    let c = 'z';
+    let z = "zzzz";
+    let heart_eyed_cat = '😻';
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {}", x); // 12
-    }
+    println!("{}", c);
+    println!("{}", z);
+    println!("{}", heart_eyed_cat);
 
-    println!("The value of x is: {}", x); // 6
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    println!("The value of z is: {}", z);
+
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one = tup.2;
+
+    println!("The value of five_hundred is: {}", five_hundred);
+    println!("The value of six_point_four is: {}", six_point_four);
+    println!("The value of one is: {}", one);
+
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("値の読み込みに失敗しました");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("入力された値は数字ではありません");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is; {}",
+        index, element
+    );
 }
