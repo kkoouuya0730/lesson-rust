50cb8a9c62d911f34abeca62138422ccfc86d52d
kkoouuya0730 <kouya.0730@icloud.com>
2026-01-24 11:18:44 +0900

    構造体-メソッド

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,46 @@
 // 構造体
-fn main() {
-    let mut user = User {
-        email: String::from("some@example.com"),
-        username: String::from("test"),
-        active: true,
-        sign_in_count: 1,
-    };
-    user.email = String::from("test@example.com");
 
-    let email = String::from("some@example.com");
-    let username = String::from("test");
-
-    let user2 = build_user(email, username);
-    let user3 = User {
-        email: String::from("some@example.com"),
-        username: String::from("test"),
-        ..user
-    };
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
-        active: true,
-        sign_in_count: 1,
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    let square = Rectangle::square(2);
+
+    // rect1にrect2ははまり込む？
+    // println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    // println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("Square {:#?}", square);
 }
