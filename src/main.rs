8f0a848d22b27f138ef1f1ddd9996ef8a61cb4b5
kkoouuya0730 <kouya.0730@icloud.com>
2026-01-24 23:32:48 +0900

    enum

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,16 @@
-// 構造体
-
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-
-    fn square(size: u32) -> Rectangle {
-        Rectangle {
-            width: size,
-            height: size,
-        }
-    }
-}
-
+// enum
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
-    };
+    let m = Message::Write(String::from("Hello"));
+    m.call();
+}
 
-    let square = Rectangle::square(2);
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
 
-    // rect1にrect2ははまり込む？
-    // println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    // println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
-    println!("Square {:#?}", square);
+impl Message {
+    fn call(&self) {}
 }
