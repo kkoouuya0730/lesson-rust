5ecb5421f267532bc786f68083e246e9fc56e192
kkoouuya0730 <kouya.0730@icloud.com>
2026-01-28 22:09:13 +0900

    ベクタ ハッシュマップ

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,50 @@
-use std::vec;
+use std::{collections::HashMap, hash::Hash};
 
-// ベクタ
+// ベクタ ハッシュマップ
 fn main() {
-    // let data = "initial contents";
+    let mut scores = HashMap::new();
 
-    // let s = data.to_string();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
 
-    // let s = "initial contents".to_string();
+    // let team_name = String::from("Blue");
+    let score = scores.get("Blue");
+    // for (key, value) in &scores {
+    //     println!("{}: {}", key, value);
+    // }
+    // let score = scores.get(&team_name);
 
-    // let mut s = String::from("foo");
-    // s.push_str("bar");
-    // s.push('b');
-    // println!("The value of s is: {}", s);
+    // let teams = vec![String::from("Blue"), String::from("Yellow")];
+    // let initial_scores = vec![10, 50];
 
-    let mut s1 = String::from("foo");
-    let s2 = "bar";
-    s1.push_str(s2);
-    println!("s2 is {}", s2);
+    // let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
 
-    // 文字列に添え字でアクセスできない
-    let s3 = String::from("hello");
-    // let h = s3[0];
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
 
-    let hello = "Здравствуйте";
-    let s = &hello[0..4];
-    // let s = &hello[0..1];
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
 
-    for c in "नमस्ते".chars() {
-        println!("{}", c);
+    // 値を上書きする
+    let mut scores2 = HashMap::new();
+    scores2.insert(String::from("Blue"), 20);
+    scores2.insert(String::from("Blue"), 24);
+
+    println!("{:?}", scores2);
+
+    // キーに値がなかった時のみ値を挿入する
+    let mut scores3 = HashMap::new();
+    scores3.insert(String::from("Blue"), 10);
+    scores3.entry(String::from("Yellow")).or_insert(50);
+    scores3.entry(String::from("Blue")).or_insert(50);
+    println!("{:?}", scores3);
+
+    // 古い値に基づいて値を更新する
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
     }
-
-    for c in "नमस्ते".bytes() {
-        println!("{}", c);
-    }
+    println!("{:?}", map);
 }
