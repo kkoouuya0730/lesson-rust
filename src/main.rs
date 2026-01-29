e542be8030dde12cf9385d6749e31f865341053e
kkoouuya0730 <kouya.0730@icloud.com>
2026-01-30 00:06:11 +0900

    ?演算子

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
-use std::{fs::File, io::ErrorKind};
-
+use std::fs::File;
+use std::io;
+use std::io::Read;
 // Result
 fn main() {
+    // let f = File::open("hello.txt").unwrap();
+    // let f = File::open("hello.txt").expect("Failed to open hello.txt");
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
     let f = File::open("hello.txt");
 
-    let f = match f {
+    let mut f = match f {
         Ok(file) => file,
-        Err(error) if error.kind() == ErrorKind::NotFound => match File::create("hello.txt") {
-            Ok(fc) => fc,
-            Err(e) => {
-                panic!("Tried to create file but there was a problem: {:?}", e);
-            }
-        },
-        Err(error) => {
-            panic!("There was a problem opening the file: {:?}", error)
-        }
+        Err(e) => return Err(e),
     };
+
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => (e),
+    }
+}
+
+fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut s = String::new();
+    let mut f = File::open("hello.txt")?.read_to_string(&mut s)?;
+    // let mut s = String::new();
+    // f.read_to_string(&mut s)?;
+    Ok(s)
 }
