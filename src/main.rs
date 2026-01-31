ea6b54a9940d515f81bdd625c10e1d11468c1725
kkoouuya0730 <kouya.0730@icloud.com>
2026-01-31 22:16:31 +0900

    ジェネリック

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,23 @@
-use std::fs::File;
-use std::io;
-use std::io::Read;
-// Result
+// ジェネリック
 fn main() {
-    // let f = File::open("hello.txt").unwrap();
-    // let f = File::open("hello.txt").expect("Failed to open hello.txt");
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
 
-fn read_username_from_file() -> Result<String, io::Error> {
-    let f = File::open("hello.txt");
-
-    let mut f = match f {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
-
-    let mut s = String::new();
-    match f.read_to_string(&mut s) {
-        Ok(_) => Ok(s),
-        Err(e) => (e),
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
     }
-}
 
-fn read_username_from_file2() -> Result<String, io::Error> {
-    let mut s = String::new();
-    let mut f = File::open("hello.txt")?.read_to_string(&mut s)?;
-    // let mut s = String::new();
-    // f.read_to_string(&mut s)?;
-    Ok(s)
+    largest
 }
