26105ec7c9982f51034e0165c3a94e6ca9e4b4b6
kkoouuya0730 <kouya.0730@icloud.com>
2026-02-04 23:11:49 +0900

    イテレータ

--- src/adder/src/lib.rs
+++ src/adder/src/lib.rs
@@ -1,89 +1,35 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+struct Counter {
+    count: u32,
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-
-//     #[test]
-//     fn another() {
-//         //このテストを失敗させる
-//         panic!("Make this test fail");
-//     }
-// }
-
-// #[derive(Debug)]
-// struct Rectangle {
-//     width: u32,
-//     height: u32,
-// }
-
-// impl Rectangle {
-//     fn can_hold(&self, other: &Rectangle) -> bool {
-//         self.width < other.width && self.height > other.height
-//     }
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn larger_can_hold_smaller() {
-//         let larger = Rectangle {
-//             width: 8,
-//             height: 7,
-//         };
-
-//         let smaller = Rectangle {
-//             width: 5,
-//             height: 1,
-//         };
-
-//         assert!(larger.can_hold(&smaller));
-//     }
-
-//     #[test]
-//     fn smaller_cannot_hold_larger() {
-//         let larger = Rectangle {
-//             width: 8,
-//             height: 7,
-//         };
-//         let smaller = Rectangle {
-//             width: 5,
-//             height: 1,
-//         };
-
-//         assert!(!smaller.can_hold(&larger));
-//     }
-// }
-
-fn prints_and_returns_10(a: i32) -> i32 {
-    //{}という値を得た
-    println!("I got the value {}", a);
-    10
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Iterator for Counter {
+    type Item = u32;
 
-    #[test]
-    fn this_test_will_pass() {
-        let value = prints_and_returns_10(4);
-        assert_eq!(10, value);
-    }
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
 
-    #[test]
-    fn this_test_will_fail() {
-        let value = prints_and_returns_10(8);
-        assert_eq!(5, value);
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
     }
+}
+
+#[test]
+fn calling_next_directly() {
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
 }
