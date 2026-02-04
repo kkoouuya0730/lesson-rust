2ffa1dee21d6584e5648b00c5d841240b097c226
kkoouuya0730 <kouya.0730@icloud.com>
2026-02-04 22:12:59 +0900

    クロージャ Cacher

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,84 @@
-// ライフタイム
-fn main() {
-    let string1 = String::from("long string is long");
+// クロージャ
 
-    {
-        let string2 = String::from("xyz");
-        let result = longest(string1.as_str(), string2.as_str());
-        // 一番長い文字列は{}
-        println!("The longest string is {}", result);
+use std::thread;
+use std::time::Duration;
+
+// fn simulated_expensive_calculation(intensity: u32) -> u32 {
+//     // ゆっくり計算します
+//     println!("calculating slowly...");
+//     thread::sleep(Duration::from_secs(2));
+//     intensity
+// }
+
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
     }
 }
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() { x } else { y }
+fn main() {
+    let simulated_user_specified_value = 43;
+    let simulated_random_number = 4;
+
+    generate_workout(simulated_user_specified_value, simulated_random_number);
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    // expensive_closureは匿名関数を呼び出した結果の値ではなく、
+    // 匿名関数の定義を含むという意味
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!(
+            // 今日は{}回腕立て伏せをしてください！
+            "Today, do {} pushups!",
+            expensive_result.value(intensity)
+        );
+
+        println!(
+            // 次に、{}回腹筋をしてください！
+            "Next, do {} situps!",
+            expensive_result.value(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            // 今日は休憩してください！水分補給を忘れずに！
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                // 今日は、{}分間走ってください！
+                "Today, run for {} minutes!",
+                expensive_result.value(intensity)
+            );
+        }
+    }
 }
