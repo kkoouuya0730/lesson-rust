c775080930c22c8966e09bc4e7460dd4b25de5d7
kkoouuya0730 <kouya.0730@icloud.com>
2026-02-05 23:29:23 +0900

    参照外し型強制

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
-// スマートポインタ Box
+// スマートポインタ 参照外し型強制
+use std::ops::Deref;
 
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
 }
 
-use List::{Cons, Nil};
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
 
 fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    // let x = 5;
+    // let y = MyBox::new(x);
+
+    // assert_eq!(5, x);
+    // assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
 }
