efd612d0a249be03294420f1a8be6f2e7eb4f76f
kkoouuya0730 <kouya.0730@icloud.com>
2026-02-06 22:28:02 +0900

    Rc<T>

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,23 @@
-// スマートポインタ drop
-struct CustomSmartPointer {
-    data: String,
+// スマートポインタ Rc<T>
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
 }
 
-impl Drop for CustomSmartPointer {
-    fn drop(&mut self) {
-        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
-    }
-}
+use List::{Cons, Nil};
+use std::rc::Rc;
 
 fn main() {
-    // let c = CustomSmartPointer {
-    //     data: String::from("my stuff"),
-    // };
-    // let d = CustomSmartPointer {
-    //     data: String::from("other stuff"),
-    // };
-    // println!("CustomSmartPointers created.");
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
 
-    let c = CustomSmartPointer {
-        data: String::from("some data"),
-    };
-    println!("CustomSmartPointer created.");
-    drop(c);
-    // mainの終端の前にCustomSmartPointerがドロップされた
-    println!("CustomSmartPointer dropped before the end of main.");
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+
+    {
+        let c = Cons(4, Rc::clone(&a));
+        // c生成後のカウント = {}
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
