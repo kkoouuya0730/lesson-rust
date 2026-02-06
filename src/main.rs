2e380b44430338accd00991aa471b133eaf0d418
kkoouuya0730 <kouya.0730@icloud.com>
2026-02-06 21:42:11 +0900

    Dropトレイト

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,28 @@
-// スマートポインタ 参照外し型強制
-use std::ops::Deref;
-
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
+// スマートポインタ drop
+struct CustomSmartPointer {
+    data: String,
 }
 
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        &self.0
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
     }
-}
-
-fn hello(name: &str) {
-    println!("Hello, {}!", name);
 }
 
 fn main() {
-    // let x = 5;
-    // let y = MyBox::new(x);
+    // let c = CustomSmartPointer {
+    //     data: String::from("my stuff"),
+    // };
+    // let d = CustomSmartPointer {
+    //     data: String::from("other stuff"),
+    // };
+    // println!("CustomSmartPointers created.");
 
-    // assert_eq!(5, x);
-    // assert_eq!(5, *y);
-
-    let m = MyBox::new(String::from("Rust"));
-    hello(&m);
+    let c = CustomSmartPointer {
+        data: String::from("some data"),
+    };
+    println!("CustomSmartPointer created.");
+    drop(c);
+    // mainの終端の前にCustomSmartPointerがドロップされた
+    println!("CustomSmartPointer dropped before the end of main.");
 }
