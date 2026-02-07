787b4eedf99d0df03e459729390a37362d1db521
kkoouuya0730 <kouya.0730@icloud.com>
2026-02-07 11:02:30 +0900

    メッセージ受け渡し

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,40 @@
-// スレッド
-
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
-    // thread::spawn(|| {
-    //     for i in 1..10 {
-    //         println!("やあ！立ち上げたスレッドから数字{}だよ！", i);
-    //         thread::sleep(Duration::from_millis(1));
-    //     }
-    // });
+    let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
 
-    // for i in 1..5 {
-    //     println!("メインスレッドから数字{}だよ！", i);
-    //     thread::sleep(Duration::from_millis(1));
-    // }
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
 
-    // let handle = thread::spawn(|| {
-    //     for i in 1..10 {
-    //         println!("hi number {} from the spawned thread!", i);
-    //         thread::sleep(Duration::from_millis(1));
-    //     }
-    // });
-
-    // for i in 1..5 {
-    //     println!("hi number {} from the main thread!", i);
-    //     thread::sleep(Duration::from_millis(1));
-    // }
-
-    // // handleが表すスレッドが終了するまで、現在実行中のスレッドをブロックする
-    // handle.join().unwrap();
-
-    let v = vec![1, 2, 3];
-
-    let handle = thread::spawn(move || {
-        println!("Here's a vector: {:?}", v);
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
-    handle.join().unwrap();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
