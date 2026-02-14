ffc2e9b5be23ed97af97af782293481930dc6992
kkoouuya0730 <kouya.0730@icloud.com>
2026-02-14 09:56:11 +0900

    サーバー

--- actix-web/src/main.rs
+++ actix-web/src/main.rs
@@ -1,89 +1,21 @@
-use std::sync::Mutex;
-
-use actix_web::{get, guard, post, web, App, HttpResponse, HttpServer, Responder};
-
-struct AppState {
-    app_name: String,
-}
-
-struct AppStateWithCounter {
-    counter: Mutex<i32>,
-}
-
-async fn index2(data: web::Data<AppStateWithCounter>) -> String {
-    let mut counter = data.counter.lock().unwrap();
-    *counter += 1;
-
-    format!("Request number: {counter}")
-}
+use actix_web::{get, App, HttpRequest, HttpServer, Responder};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 #[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name;
-    format!("Hello {app_name}!")
-}
-#[get("/ping")]
-async fn ping() -> impl Responder {
-    HttpResponse::Ok().body("pong")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("hey there")
-}
-
-fn scoped_config(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("/test")
-            .route(web::get().to(|| async { HttpResponse::Ok().body("test") }))
-            .route(web::head().to(HttpResponse::MethodNotAllowed)),
-    );
-}
-
-fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("/app")
-            .route(web::get().to(|| async { HttpResponse::Ok().body("app") }))
-            .route(web::head().to(HttpResponse::MethodNotAllowed)),
-    );
+async fn index(_req: HttpRequest) -> impl Responder {
+    "Welcome!"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let counter = web::Data::new(AppStateWithCounter {
-        counter: Mutex::new(0),
-    });
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    builder
+        .set_private_key_file("key.pem", SslFiletype::PEM)
+        .unwrap();
+    builder.set_certificate_chain_file("cert.pem").unwrap();
 
-    HttpServer::new(move || {
-        App::new()
-            .service(
-                web::scope("/")
-                    .guard(guard::Header("Host", "www.rust-lang.org"))
-                    .route("", web::to(|| async { HttpResponse::Ok().body("www") })),
-            )
-            .service(
-                web::scope("/")
-                    .guard(guard::Header("Host", "users.rust-lang.org"))
-                    .route("", web::to(|| async { HttpResponse::Ok().body("www") })),
-            )
-            .app_data(counter.clone())
-            .route("/", web::get().to(index2))
-            .service(index)
-            .service(ping)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-            .configure(config)
-            .service(web::scope("/api").configure(scoped_config))
-            .route(
-                "/",
-                web::get().to(|| async { HttpResponse::Ok().body("/") }),
-            )
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(index))
+        .bind_openssl("127.0.0.1:8080", builder)?
+        .run()
+        .await
 }
